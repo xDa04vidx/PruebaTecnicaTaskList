66aa2e7abae795de93d306951b8ceabf4d9a2b53
stivenpow <stivenpow@gmail.com>
2025-11-26 16:32:58 -0500

    pusible error del commit

--- API_Django/tasks/urls.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from .viewsets import TasksViewSet
-
-router = DefaultRouter()
-router.register('tasks',TasksViewSet)
-
-urlpatterns =  router.urls
--- API_Django/tasks/viewsets.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import viewsets
-from .serializer import TaskSerializer
-from .models import Tasks
-
-class TasksViewSet(viewsets.ModelViewSet):
-    serializer_class= TaskSerializer
-    queryset= Tasks.objects.all()
-
-    
